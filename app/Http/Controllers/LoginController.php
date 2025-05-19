78a309cafea089f9b014eb7022d5e3bee4ed4b6d
grupopi25 <grupopiii27@gmail.com>
2025-05-19 02:15:08 -0300

    adicionado o back-end de login. login funcionando perfeitamente.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function login(){
         return view('user.login');
+    }
+
+    public function authenticate(Request $request) {
+
+            $credentials = $request->validate([
+            'email'    => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('home')->with('success','Seja Bem-Vindo!');
+        }
+          return back()->withInput()->with('error','Usuário não Encontrado no banco de dados!');
+
+
+
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home',[HomeController::class,'home'])->name('home');
 
 Route::get('/',[WelcomeController::class,'index'])->name('welcome');
-Route::get('/login',[LoginController::class,'login'])->name('login');
+
 Route::get('/cadastro',[CadastroUserController::class,'index'])->name('cadastro');
 Route::post('/store-user',[CadastroUserController::class,'store'])->name('store-user');
+
+Route::get('/login',[LoginController::class,'login'])->name('login');
+Route::post('/auth',[LoginController::class,'authenticate'])->name('user-login');
 
 
 
