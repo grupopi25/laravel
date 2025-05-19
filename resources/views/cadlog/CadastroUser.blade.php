e31ff9ae2b1ab3bd3820362d2da3e6edbc22b7ab
grupopi25 <grupopiii27@gmail.com>
2025-05-18 23:40:28 -0300

    adicionado mais duas telas a de login e a de cadastro  (cadastro ainda falta terminar)

--- resources/views/cadlog/CadastroUser.blade.php
+++ resources/views/cadlog/CadastroUser.blade.php
@@ -1,4 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/cadlog/cadastroUser.css') }}">
+    <title>Cadastro</title>
+</head>
+<body>
+<div class="containner">
+      <form>
 
-<div>
-   <h1>Cadastro  </h1>
+        <h1>
+            Cadastra-se
+        </h1>
+        <div class="box-containner">
+            <input type="text" placeholder="Nome" required>
+        </div>
+        <div class="box-containner">
+            <input type="email" placeholder="Email" required>
+        </div>
+        <div class="box-containner">
+            <input type="password" placeholder="Senha" required>
+            <input type="checkbox" id="check"><span id="termo">Politica de Privacidade</span>
+        </div>
+
+
+        <button type="submit" class="btn-submit">Enviar</button>
+
+
+
+      </form>
+
 </div>
+
+</body>
+</html>
--- resources/views/cadlog/login.blade.php
+++ resources/views/cadlog/login.blade.php
@@ -1,4 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/cadlog/cadastroUser.css') }}">
+    <title>Login</title>
+</head>
+<body>
+<div class="containner login">
+      <form>
 
-<div>
-  <h1 >Login </h1>
+        <h1>
+            Login
+        </h1>
+        <div class="box-containner">
+            <input type="text" placeholder="Email" required>
+            <img width="20" height="20" src="https://img.icons8.com/ios-filled/50/ffffff/user--v1.png" alt="user"/>
+        </div>
+
+        <div class="box-containner">
+            <input type="password" placeholder="Senha" required>
+         <img width="20" height="20" src="https://img.icons8.com/material/24/ffffff/lock--v1.png" alt="lock"/>
+         <a href="#">Esqueci minha senha</a>
+        </div>
+
+        <button type="submit" class="btn-submit">Enviar</button>
+
+        <div class="register">
+            <p>
+                Não está cadastrado  ? <a href="{{ route('cadastro') }}">Cadastrar</a>
+            </p>
+        </div>
+
+      </form>
+
 </div>
+
+</body>
+</html>
